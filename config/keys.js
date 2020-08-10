8b373c1da065bad4ddea98935afe365e62e23071
William Shelley <wjshelley5018@gmail.com>
2020-08-09 23:17:28 -0400

    Hidden keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dev:jWoIgV2VxlpI53pF@cluster0.nwabl.mongodb.net/wager?retryWrites=true&w=majority",
-    secretOrKey: '4G#H&-F?{Y~5vc:0HE9L*^^sS0x[5H'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
