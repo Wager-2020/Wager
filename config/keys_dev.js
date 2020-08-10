281e23fa1e91e3d2cdec24c697572698eedaef9e
William Shelley <wjshelley5018@gmail.com>
2020-08-09 23:27:12 -0400

    Cleared Cached

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://dev:jWoIgV2VxlpI53pF@cluster0.nwabl.mongodb.net/wager?retryWrites=true&w=majority",
-    secretOrKey: '4G#H&-F?{Y~5vc:0HE9L*^^sS0x[5H'
-};
