69ab7e181ff11308e38763ecb0a89235cd8f091c
SVKratzer <samuel.kratzer1894@gmail.com>
2020-08-25 16:35:26 -0400

    Create demo login button that generates new user

--- frontend/src/components/session/demo_login_button.js
+++ frontend/src/components/session/demo_login_button.js
@@ -1,2 +1,33 @@
 import React from 'react';
+import faker from 'faker';
 
+class DemoLoginButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit= this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let _demoUser = {
+      handle: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: 'password',
+      password2: 'password'
+    };
+    this.props.signup(_demoUser);
+  }
+
+
+  render() {
+    return (
+      <button 
+        className="demo-login"
+        onClick={this.handleSubmit}>
+        Demo Login
+      </button>
+    );
+  }
+}
+
+export default DemoLoginButton;
